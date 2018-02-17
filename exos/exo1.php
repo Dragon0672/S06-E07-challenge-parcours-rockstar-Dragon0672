1506a39f985b0da476e0138400cdededdbb879fb
Dragon0672 <destd@laposte.net>
2018-02-17 16:35:37 +0100

    1/4

--- exos/exo1.php
+++ exos/exo1.php
@@ -1,4 +1,7 @@
 <?php
+// require(__DIR__.'\vendor\altorouter\altorouter\AltoRouter.php');
+// require(__DIR__.'../controllers/CoreController.php');
+
 require_once '../inc/functions.php';
 require '../vendor/autoload.php';
 
@@ -21,9 +24,39 @@ require '../vendor/autoload.php';
  *      en passant "controller#methode"
  */
 
-$router = new AltoRouter();
-
-
+ // On instancie un nouvel objet de class AltoRouter
+ $router = new AltoRouter();
+ // Pour  l'URL de base, si le fichier .htaccess est présent alors
+// Si $_SERVER['BASE_URI'] existe (grâce à la présence du fichier .htaccess),
+// on récupère l'URL de base sinon,  "/"
+ $basePath = isset($_SERVER['BASE_URI'])?$_SERVER['BASE_URI']:'/';
+// On définit l'URL de base récupéré précédement
+ $router->setBasePath($basePath);
+ // ----------------
+ // ROUTE = le chemin d'une URL à la méthode du controller
+ // ----------------
+ $router->map('GET', '/','ConcertController#enVoiture', 'home');
+ $match = $router->match();
+ // ----------------
+ // DISPATCHER
+ // ----------------
+ // Si le chemin existe
+ if($match !== false) {
+   // On récupère le nom du controller et de sa method dans un tableau indexé
+  $controllerParts = explode('#', $match['target']);
+  // On affecte le nom du controller
+  $controllerName = $controllerParts[0];
+  // On affecte le nom de la method du controller
+  $methodName = $controllerParts[1];
+  // On instancie un nouvel objet de class concertController
+  $controller = new $controllerName();
+  // On appzllz la mzthod enVoiture de l'objet concertController
+  $controller->$methodName($match['params']);
+ }
+ else { // Sinon...
+  // ... on informe d'une erreur de type 404
+  echo '404';
+ }
 
 /*
  * Tests
