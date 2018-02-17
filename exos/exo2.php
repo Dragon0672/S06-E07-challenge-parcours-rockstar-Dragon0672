a22d3d74bd5769aeec6813defccffa0776be5832
Dragon0672 <destd@laposte.net>
2018-02-17 20:11:44 +0100

    2/4

--- exos/exo2.php
+++ exos/exo2.php
@@ -25,6 +25,37 @@ require '../vendor/autoload.php';
  */
 
  $router = new AltoRouter();
+ // Pour  l'URL de base, si le fichier .htaccess est présent alors
+// Si $_SERVER['BASE_URI'] existe (grâce à la présence du fichier .htaccess),
+// on récupère l'URL de base sinon,  "/"
+ $basePath = isset($_SERVER['BASE_URI'])?$_SERVER['BASE_URI']:'/';
+// On définit l'URL de base récupéré précédement
+ $router->setBasePath($basePath);
+ // ----------------
+ // ROUTE = le chemin d'une URL à la méthode du controller
+ // ----------------
+ $router->map('GET', '/groupies/signer/[a:name]','ConcertController#autographe');
+ $match = $router->match();
+ // ----------------
+ // DISPATCHER
+ // ----------------
+ // Si le chemin existe
+ if($match !== false) {
+   // On récupère le nom du controller et de sa method dans un tableau indexé
+  $controllerParts = explode('#', $match['target']);
+  // On affecte le nom du controller
+  $controllerName = $controllerParts[0];
+  // On affecte le nom de la method du controller
+  $methodName = $controllerParts[1];
+  // On instancie un nouvel objet de class concertController
+  $controller = new $controllerName();
+  // On appelle la method autographe de l'objet concertController
+  $controller->$methodName($match['params']);
+ }
+ else { // Sinon...
+  // ... on informe d'une erreur de type 404
+  echo '404';
+ }
 
 
 
